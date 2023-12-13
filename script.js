b3b5ebbdab0f3bad66088fa9e0bfb45cfc5b175f
Eason5959 <152355658+Eason5959@users.noreply.github.com>
2023-12-13 13:49:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,13 @@ document.getElementById('addButton').addEventListener('click', function() {
 
         // Create the Query Result button
         var queryButton = document.createElement('button');
-        queryButton.addEventListener('click', function() {
-        // Open the new HTML page in a new tab/window
-        window.open('queryResults.html', '_blank');
-        });
         queryButton.textContent = '查詢結果';
         queryButton.classList.add('query-result-button');
+
         // Add event listener for the button
         queryButton.addEventListener('click', function() {
-            // Implement the logic for what happens when the button is clicked
-            alert('查詢結果 for ' + oldCaseNumber);
+            // Here you can implement the logic to open a new page and pass data
+            window.open('queryResults.html', '_blank');
         });
 
         listItem.appendChild(queryButton);
