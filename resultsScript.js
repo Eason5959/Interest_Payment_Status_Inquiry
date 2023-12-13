e28058944c1030728c55cf7998a5330583246df2
Eason5959 <152355658+Eason5959@users.noreply.github.com>
2023-12-13 13:46:39 +0800

    Create resultsScript.js

---
+++ resultsScript.js
@@ -0,0 +1,7 @@
+// resultsScript.js
+document.getElementById('resultA').textContent = '1,170,000';
+document.getElementById('resultB').textContent = '72';
+document.getElementById('resultC').textContent = '23';
+document.getElementById('resultD').textContent = 'ASR-9682';
+document.getElementById('resultE').textContent = '4';
+document.getElementById('resultF').textContent = '20,592';
