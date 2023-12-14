a756609781f6097c8e9e725ce199c5744c93610e
Eason5959 <152355658+Eason5959@users.noreply.github.com>
2023-12-14 14:41:40 +0800

    Update resultsScript.js

--- resultsScript.js
+++ resultsScript.js
@@ -5,3 +5,4 @@ document.getElementById('resultC').textContent = '72';
 document.getElementById('resultD').textContent = '23';
 document.getElementById('resultE').textContent = '20,592';
 document.getElementById('resultF').textContent = '4';
+document.getElementById('resultG').textContent = '112-12-14';
